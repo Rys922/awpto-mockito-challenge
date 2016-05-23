4689d27ec4679db33283961b80970d89a92aee19
Richard <Richard@155.158.172.57>
2016-05-23 17:40:30 +0200

    jajaja

--- MockitoChallenge/src/test/java/model/UserTest.java
+++ MockitoChallenge/src/test/java/model/UserTest.java
@@ -2,8 +2,11 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.security.NoSuchAlgorithmException;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import service.IHashingInterface;
 import service.MD5;
@@ -11,16 +14,31 @@ import service.MD5;
 public class UserTest {
 
 	private User _user;
+	User mockedUser;
 	
 	@Before
-	public void setup() {
+	public void setup() throws NoSuchAlgorithmException {
 		IHashingInterface hashing = new MD5();
 		_user = new User("student", "studentii", hashing);
+		_user.generateSalt("Rys");
+		
+		mockedUser = Mockito.mock(User.class);
+		Mockito.when(mockedUser.getSalt()).thenReturn(hashing.getHash(_user.getName() + "Rys"));
 	}
 	
 	@Test
 	public void testUserName() {
 		assertEquals("student", _user.getName());
+	}
+	
+	@Test
+	public void testUserPassword() {
+		assertEquals("studentii", _user.getPassword());
+	}
+	
+	@Test
+	public void testUserSalt() {
+		assertEquals(mockedUser.getSalt(), _user.getSalt());
 	}
 
 }
--- MockitoChallenge/src/test/java/service/UsersServiceTest.java
+++ MockitoChallenge/src/test/java/service/UsersServiceTest.java
@@ -2,20 +2,33 @@ package service;
 
 import static org.junit.Assert.*;
 
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import model.User;
 
 public class UsersServiceTest {
 
 	@Mock UsersService _usersService;
+	@Mock User w;
 	
 	@Before
     public void setup() {
 		MockitoAnnotations.initMocks(this);
+		_usersService = new UsersService();
+		IHashingInterface _hashing = new MD5();
+		w = Mockito.spy(new User("a", "b", _hashing));
     }
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws NoSuchAlgorithmException {
+		_usersService.add(w);
+		Mockito.verify(w, Mockito.times(1)).generateSalt(Mockito.anyString());
 	}
 
 }
